1cca9e364426f0d96db3f6a7d43d0cdc0553852b
Makeen <makeenukeg@gmail.com>
2025-10-31 16:13:54 +0200

    Solving timer_deinit problem and modifying on the motor driver

--- motor.c
+++ motor.c
@@ -2,8 +2,6 @@
 #include "gpio.h"
 #include "timer0.h"
 
-
-
 void DcMotor_Init(PWM_Timer timer)
 {
 	if(timer == TIMER0)
@@ -42,13 +40,13 @@ void DcMotor_Rotate(DcMotor_State state,uint8 speed,PWM_Timer timer)
 		{
 			GPIO_writePin(DCMOTOR1_PORTID,DCMOTOR1_PIN1ID, LOGIC_LOW);
 			GPIO_writePin(DCMOTOR1_PORTID,DCMOTOR1_PIN2ID, LOGIC_HIGH);
-			PWM_Timer0_deInit();
+			
 		}	
 		else if(timer == TIMER2)
 		{
 			GPIO_writePin(DCMOTOR2_PORTID,DCMOTOR2_PIN1ID, LOGIC_LOW);
 			GPIO_writePin(DCMOTOR2_PORTID,DCMOTOR2_PIN2ID, LOGIC_HIGH);
-			PWM_Timer2_deInit();
+			
 		}
 		break;		
 	case DCMOTOR_CW :
@@ -56,23 +54,23 @@ void DcMotor_Rotate(DcMotor_State state,uint8 speed,PWM_Timer timer)
 		{
 			GPIO_writePin(DCMOTOR1_PORTID,DCMOTOR1_PIN1ID, LOGIC_HIGH);
 			GPIO_writePin(DCMOTOR1_PORTID,DCMOTOR1_PIN2ID, LOGIC_LOW);
-			PWM_Timer0_deInit();
+			
 		}	
 		else if(timer == TIMER2)
 		{
 			GPIO_writePin(DCMOTOR2_PORTID,DCMOTOR2_PIN1ID, LOGIC_HIGH);
 			GPIO_writePin(DCMOTOR2_PORTID,DCMOTOR2_PIN2ID, LOGIC_LOW);
-			PWM_Timer2_deInit();
+			
 		}
 		break;
 	default:
 		;
 	}
-	if(timer == TIMER0)
+	if(timer == TIMER0 && state != DCMOTOR_STOP)
 	{
 		PWM_Timer0_Start(speed);
 	}
-	else if(timer == TIMER2)
+	else if(timer == TIMER2 && state != DCMOTOR_STOP)
 	{
 		PWM_Timer2_Start(speed);
 	}
